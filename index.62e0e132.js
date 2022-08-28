8629af2e93c192139484261491bb9cc6e3ac2f47
ventel90 <107477405+ventel90@users.noreply.github.com>
2022-08-28 18:39:40 +0000

    Deploying to gh-pages from @ ventel90/team11-GloryToUkraine@c1bc261edf626f675ec80b21902a02e2d8aea508 🚀

---
+++ index.62e0e132.js
@@ -0,0 +1,2 @@
+!function(){var e=document.querySelector("[data-menu-open]"),t=document.querySelector("[data-menu-close]"),c=document.querySelector("[data-menu]"),n=document.querySelector("body");function o(){c.classList.toggle("is-open"),n.classList.toggle("no-scroll")}e.addEventListener("click",o),t.addEventListener("click",o)}();
+//# sourceMappingURL=index.62e0e132.js.map
---
+++ index.93a0efee.js
@@ -0,0 +1,2 @@
+function changeHeaderBackground(){var e=document.querySelector("[data-header]"),a=e.offsetTop;window.pageYOffset>a?e.classList.add("header--no-transparency"):e.classList.remove("header--no-transparency")}window.onscroll=function(){return changeHeaderBackground()};
+//# sourceMappingURL=index.93a0efee.js.map
